da8f04c44ab51422bd9701f2d3a57aeb01a82a06
Michael Aguirre Saavedra <michael.aguirre@epchinchorro.cl>
2025-11-25 17:18:52 -0300

    update postulaciones 2

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-if (!window.location.href.includes("localhost")) {
+/* if (!window.location.href.includes("localhost")) {
   import.meta.env.VITE_API_URL = "http://10.200.1.145:8000/api";
-}
+} */
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
